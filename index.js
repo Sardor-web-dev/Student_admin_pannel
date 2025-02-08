0e645f58a7f80b5acdd986eb7ae58d9b78625da5
SarikViper <dzamolovsardor5@gmail.com>
2025-02-08 18:04:37 +0500

    Some changes

--- index.js
+++ index.js
@@ -12,12 +12,12 @@
     formGroup.className = "form-group";
 
     let inputName = document.createElement("input");
-    inputName.setAttribute('name, name')
+    inputName.setAttribute("name", "username");
     inputName.placeholder = "Имя";
     formGroup.append(inputName);
 
     let inputAge = document.createElement("input");
-    inputAge.setAttribute('name, age')
+    inputAge.setAttribute("name", "age");
     inputAge.placeholder = "Возраст";
     inputAge.type = "number";
     formGroup.append(inputAge);
